c8193a766aa337ef8b46410fbd7ed2945dbc6a02
Azin Makaranth <azin.makaranth@gmail.com>
2021-11-27 18:43:57 +0000

    overallStructure

---
+++ vechicle.py
@@ -0,0 +1,21 @@
+
+
+class vechile{
+    broadCastSystem,
+    control&Sensors
+}
+class  infra{
+    broadCastSystembroadCastSystem
+    control&Sensors
+    extra function {
+
+    }
+}
+
+
+main() {
+    new vechile()
+
+    t1 = vechile.getThread1()
+
+}
