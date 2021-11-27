0426b6c0c1f2d10903d281e287d80cb34bf0e31f
OmkarPadir <omkar.padir@gmail.com>
2021-11-27 19:46:13 +0000

    change in function name
    Changed function name for consistency.
    Added class Sensor and function GetSensors() that returns a list of sensor objects

--- Sensor_Data_Generation.py
+++ Sensor_Data_Generation.py
@@ -29,7 +29,7 @@ class PressureSensor:
 
         return INITIAL_VALUE
 
-    def GET_PRESSURE(self):
+    def GET_DATA(self):
         randvalue2 = randint(0,10000)
         if randvalue2 <= 9998:  # return same as initial value
             self.PRESSURE = self.INITIAL_PRESSURE
@@ -40,7 +40,7 @@ class PressureSensor:
             self.PRESSURE = self.INITIAL_PRESSURE + 1
             self.INITIAL_PRESSURE = self.INITIAL_PRESSURE + 1
 
-        return self.PRESSURE
+        return ['TP',self.PRESSURE]
 
 class SpeedSensor:
 
@@ -54,7 +54,7 @@ class SpeedSensor:
         self.INITIAL_SPEED = randint(40,80)
         return self.INITIAL_SPEED
 
-    def GET_SPEED(self,param_FLAG='DEFAULT'):
+    def GET_DATA(self,param_FLAG='DEFAULT'):
 
         if(self.TICKS == 0):
             self.FLAG=param_FLAG
@@ -89,10 +89,10 @@ class SpeedSensor:
                 if(self.TICKS == 5):
                     self.TICKS=0
                     self.FLAG='DEFAULT'
-        print("-----------")
-        print("TICKS: ",self.TICKS," FLAG: ",self.FLAG)
-        print("-----------")
-        return self.SPEED
+        # print("-----------")
+        # print("TICKS: ",self.TICKS," FLAG: ",self.FLAG)
+        # print("-----------")
+        return ['SPD',self.SPEED]
 
 
 
@@ -103,7 +103,7 @@ class LightSensor():
         self.d1 = datetime(2020, 5, 13, 8, 00, 00)
         self.d2 = datetime(2020, 5, 13, 17, 00, 00)
 
-    def GET_LIGHT(self):
+    def GET_DATA(self):
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         # print("Current Time =", current_time)
@@ -113,7 +113,7 @@ class LightSensor():
         else:
             self.LIGHT='LOW'
 
-        return self.LIGHT
+        return ['LT',self.LIGHT]
 
 
 class FuelSensor():
@@ -123,12 +123,14 @@ class FuelSensor():
         self.FUEL=self.INITIAL_FUEL
         self.TICKS=0
 
-    def GET_FUEL(self):
+    def GET_DATA(self):
 
         if( self.TICKS % 50 == 0):
-            self.FUEL = -1
+            self.FUEL -= 1
 
         self.TICKS+=1
+
+        return ['FLG',self.FUEL]
 
     def REFILL_FUEL(self,REFILL_PERCENT):
         self.FUEL += REFILL_PERCENT
@@ -136,8 +138,33 @@ class FuelSensor():
         if(self.FUEL >100):
             self.FUEL = 100
 
+class Sensor:
+    def GetSensors(self):
+        p1 = PressureSensor()
+        s1 = SpeedSensor()
+        l1 = LightSensor()
+        f1 = FuelSensor()
+
+        sensorObjects = []
+        sensorObjects.append(p1)
+        sensorObjects.append(s1)
+        sensorObjects.append(l1)
+        sensorObjects.append(f1)
+
+        return sensorObjects
 
 def GET_SENSOR_DATA():
+
+
+    S = Sensor()
+    Sobj = S.GetSensors()
+
+    for s in Sobj:
+        print(s.GET_DATA())
+
+
+
+    # This is just for TESTING
 
     p1 = PressureSensor()
     s1 = SpeedSensor()
@@ -146,21 +173,21 @@ def GET_SENSOR_DATA():
 
     for _ in range(0,100):
         time.sleep(1)
-        PRESSURE = p1.GET_PRESSURE()
+        PRESSURE = p1.GET_DATA()
 
         if(_%20 ==0):
-            SPEED = s1.GET_SPEED('DECREASE')    #Trial for decrease
+            SPEED = s1.GET_DATA('DECREASE')    #Trial for decrease
         else:
-            SPEED = s1.GET_SPEED('DEFAULT')
+            SPEED = s1.GET_DATA('DEFAULT')
 
-        LIGHT = l1.GET_LIGHT()
+        LIGHT = l1.GET_DATA()
 
-        FUEL = f1.GET_FUEL()
+        FUEL = f1.GET_DATA()
 
-        print("PRESSURE: "+str(PRESSURE))
-        print("SPEED: "+str(SPEED))
-        print("LIGHT: "+str(LIGHT))
-        print("FUEL: "+str(FUEL))
+        print("PRESSURE: "+str(PRESSURE[1]))
+        print("SPEED: "+str(SPEED[1]))
+        print("LIGHT: "+str(LIGHT[1]))
+        print("FUEL: "+str(FUEL[1]))
 
 
 GET_SENSOR_DATA()
