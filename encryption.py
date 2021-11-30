44d378e6d5cc60ee72e66dfa320e69857eddb95e
Rakesh N <ranair@tcd.ie>
2021-11-30 05:04:33 +0000

    commenting unwanted/unused snippet

--- encryption.py
+++ encryption.py
@@ -7,8 +7,8 @@ from Crypto.Cipher import AES
 
 
 def do_encrypt(message):
-    while len(bytes(message, encoding='utf-8')) % 16 != 0:
-        message = message + random.choice(string.ascii_letters)
+#    while len(bytes(message, encoding='utf-8')) % 16 != 0:
+#        message = message + random.choice(string.ascii_letters)
     obj = AES.new('This is a key133', AES.MODE_CBC, 'This is an IV456')
     ciphertext = obj.encrypt(message)
     return ciphertext
