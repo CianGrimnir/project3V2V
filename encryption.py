ddc81b257eeb723656afc1db3e92e966ad3f2126
Rakesh N <ranair@tcd.ie>
2021-11-30 06:51:56 +0000

    fixed another issue with padding (；・∀ ・)

--- encryption.py
+++ encryption.py
@@ -1,9 +1,11 @@
+
+# uses pycryptodome package 
 import Crypto.Cipher.AES as AES
 from Crypto.PublicKey import RSA
 
 import string
 import random
-from Crypto.Cipher import AES
+#from Crypto.Cipher import AES
 import base64
 from Crypto import Random
 import os, hashlib
@@ -15,15 +17,21 @@ unpadding = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
 def do_encrypt(message):
-    IV = hashlib.md5(os.urandom(128)).hexdigest()[:16]
+    # IV = hashlib.md5(os.urandom(128)).hexdigest()[:16]
+    IV = Random.new().read(AES.block_size)
     cipher = AES.new('abcd1234efgh5678'.encode("utf8"), AES.MODE_CBC, IV)
     message = padding(message)
-    ciphertext = cipher.encrypt(message)
+    #ciphertext = cipher.encrypt(message)
+    encrypted_message = cipher.encrypt(message.encode("utf-8"))
+    ciphertext = base64.b64encode(IV + encrypted_message)
     return ciphertext
 
 
 def do_decrypt(ciphertext):
-    IV = hashlib.md5(os.urandom(128)).hexdigest()[:16]
-    cipher = AES.new('abcd1234efgh5678'.encode("utf8"), AES.MODE_CBC, IV)
-    message = unpadding(cipher.decrypt(ciphertext))
+    enc = base64.b64decode(ciphertext)
+    iv = enc[:16]
+    #IV = hashlib.md5(os.urandom(128)).hexdigest()[:16]
+    cipher = AES.new('abcd1234efgh5678'.encode("utf8"), AES.MODE_CBC, iv)
+    message = unpadding(cipher.decrypt(enc[16:]))
+    #message = unpadding(cipher.decrypt(ciphertext))
     return message
