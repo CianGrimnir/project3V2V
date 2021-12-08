ff91665a8b2bdac102aa13c642f2caac6f6ae3ca
Rakesh N <ranair@tcd.ie>
2021-12-08 16:49:12 +0000

    Removed duplicate code.

--- vehicle.py
+++ vehicle.py
@@ -1,22 +1,24 @@
-import control as ctrl 
+import control as ctrl
 import argparse
 import socket
 import threading
 import time
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
+
 @app.route('/vehicle/sensor_controls/speed/<string:value>')
 def speedControl(value):
     vehicle = Vehicle.getInstance()
-    if value == "D" :
+    if value == "D":
         vehicle.sensorMaster.setSpeedSensor("DECREASE")
-    elif value == "I" :
+    elif value == "I":
         vehicle.sensorMaster.setSpeedSensor("INCREASE")
-    else :
+    else:
         vehicle.sensorMaster.setSpeedSensor("DEFAULT")
     return "Signal generated"
+
 
 @app.route('/vehicle/sensor_controls/brake/<string:value>')
 def brakeControl(value):
@@ -30,34 +32,25 @@ class Vehicle(ctrl.VehicleControls):
 
     @staticmethod
     def getInstance():
-      return Vehicle.__instance
+        return Vehicle.__instance
 
     def __init__(self, vehicle_id, host_address, listening_port, sending_port, latitude, longitude):
         super().__init__(vehicle_id, host_address, listening_port, sending_port, latitude, longitude)
         Vehicle.__instance = self
 
-class Vehicle( ctrl.VehicleControls) :
-    __instance = None
+    def deploy(self):
+        return super().deploy()
 
-    @staticmethod
-    def getInstance():
-      return Vehicle.__instance
 
+class Infra(ctrl.InfraControls):
     def __init__(self, vehicle_id, host_address, listening_port, sending_port):
         super().__init__(vehicle_id, host_address, listening_port, sending_port)
-        Vehicle.__instance = self
 
     def deploy(self):
         return super().deploy()
 
-class Infra( ctrl.InfraControls) :
-    def __init__(self, vehicle_id, host_address, listening_port, sending_port):
-        super().__init__(vehicle_id, host_address, listening_port, sending_port)
-    
-    def deploy(self):
-        return super().deploy()
-    
-def Main() :
+
+def Main():
     my_parser = argparse.ArgumentParser(description='command to execute the ./server script')
     my_parser.add_argument('--listen_port', help='listening_port', required=True)
     my_parser.add_argument('--sending_port', help='sending_port', required=True)
@@ -65,23 +58,21 @@ def Main() :
     my_parser.add_argument('--node_type', help='node_type', required=False)
     my_parser.add_argument('--api_port', help='api_port', required=False)
 
-
     args = my_parser.parse_args()
     hostname = socket.gethostname()
     host = socket.gethostbyname(hostname)
     is_infra = False
-    if args.node_type != None : 
+    if args.node_type != None:
         is_infra = args.node_type == 'I' or args.node_type == "i"
     if not is_infra:
         print("Vehicle-", args.vehicle_id)
-        get_vehicle = Vehicle( int(args.vehicle_id), host, int(args.listen_port), int(args.sending_port))
+        get_vehicle = Vehicle(int(args.vehicle_id), host, int(args.listen_port), int(args.sending_port))
         get_vehicle.deploy()
-    else :
-        print("isInfra-",args.vehicle_id)
-        get_infra = Infra( int(args.vehicle_id), host, int(args.listen_port), int(args.sending_port))
+    else:
+        print("isInfra-", args.vehicle_id)
+        get_infra = Infra(int(args.vehicle_id), host, int(args.listen_port), int(args.sending_port))
         get_infra.deploy()
     app.run(host='localhost', port=int(args.api_port))
-
 
 
 if __name__ == '__main__':
